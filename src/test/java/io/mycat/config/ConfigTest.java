0a3609ba09c4a13c84cef48e2494fa537fccdee5
qianbw <biweiqian@webank.com>
2017-02-15 16:04:07 +0800

    delete catlet

--- src/test/java/io/mycat/config/ConfigTest.java
+++ src/test/java/io/mycat/config/ConfigTest.java
@@ -1,14 +1,7 @@
 package io.mycat.config;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
-
 import io.mycat.backend.datasource.PhysicalDBPool;
 import io.mycat.backend.datasource.PhysicalDatasource;
-import io.mycat.backend.jdbc.JDBCDatasource;
 import io.mycat.backend.mysql.nio.MySQLDataSource;
 import io.mycat.config.loader.ConfigLoader;
 import io.mycat.config.loader.xml.XMLConfigLoader;
@@ -20,40 +13,47 @@ import io.mycat.config.model.SystemConfig;
 import io.mycat.config.model.TableConfig;
 import io.mycat.config.model.UserConfig;
 import io.mycat.config.util.ConfigException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.Assert;
 
+import org.junit.Test;
+
 public class ConfigTest {
-	
+
 	private SystemConfig system;
 	private final Map<String, UserConfig> users;
 	private Map<String, SchemaConfig> schemas;
-	private Map<String, PhysicalDBPool> dataHosts;	
-	
+	private Map<String, PhysicalDBPool> dataHosts;
+
 	public ConfigTest() {
-		
+
 		String schemaFile = "/config/schema.xml";
 		String ruleFile = "/config/rule.xml";
-		
+
 		XMLSchemaLoader schemaLoader = new XMLSchemaLoader(schemaFile, ruleFile);
 		XMLConfigLoader configLoader = new XMLConfigLoader(schemaLoader);
-		
+
 		this.system = configLoader.getSystemConfig();
 		this.users = configLoader.getUserConfigs();
-		this.schemas = configLoader.getSchemaConfigs();		
-        this.dataHosts = initDataHosts(configLoader);
-        
+		this.schemas = configLoader.getSchemaConfigs();
+		this.dataHosts = initDataHosts(configLoader);
+
 	}
-	
+
 	/**
 	 * 测试 临时读可用 配置
 	 */
 	@Test
 	public void testTempReadHostAvailable() {
-		PhysicalDBPool pool = this.dataHosts.get("localhost2");   
+		PhysicalDBPool pool = this.dataHosts.get("localhost2");
 		DataHostConfig hostConfig = pool.getSource().getHostConfig();
-		Assert.assertTrue( hostConfig.isTempReadHostAvailable() == true );
+		Assert.assertTrue(hostConfig.isTempReadHostAvailable() == true);
 	}
-	
+
 	/**
 	 * 测试 用户服务降级 拒连 配置
 	 */
@@ -61,39 +61,38 @@ public class ConfigTest {
 	public void testReadUserBenchmark() {
 		UserConfig userConfig = this.users.get("test");
 		int benchmark = userConfig.getBenchmark();
-		Assert.assertTrue( benchmark == 11111 );
+		Assert.assertTrue(benchmark == 11111);
 	}
-	
-	
+
 	/**
-     * 测试 读服务的 权重
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testReadHostWeight() throws Exception {
-    	
-    	ArrayList<PhysicalDatasource> okSources = new ArrayList<PhysicalDatasource>();
-    	
-    	PhysicalDBPool pool = this.dataHosts.get("localhost2");   
-    	okSources.addAll(pool.getAllDataSources());    	
-    	PhysicalDatasource source = pool.randomSelect( okSources );
-  
-    	Assert.assertTrue( source != null );
-    }
-    
-    /**
-     * 测试 动态日期表
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testDynamicYYYYMMTable() throws Exception {
-    	SchemaConfig sc = this.schemas.get("dbtest1");
-    	Map<String, TableConfig> tbm = sc.getTables();
-    	Assert.assertTrue( tbm.size() == 32);    	
-    }
-    
+	 * 测试 读服务的 权重
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testReadHostWeight() throws Exception {
+
+		ArrayList<PhysicalDatasource> okSources = new ArrayList<PhysicalDatasource>();
+
+		PhysicalDBPool pool = this.dataHosts.get("localhost2");
+		okSources.addAll(pool.getAllDataSources());
+		PhysicalDatasource source = pool.randomSelect(okSources);
+
+		Assert.assertTrue(source != null);
+	}
+
+	/**
+	 * 测试 动态日期表
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testDynamicYYYYMMTable() throws Exception {
+		SchemaConfig sc = this.schemas.get("dbtest1");
+		Map<String, TableConfig> tbm = sc.getTables();
+		Assert.assertTrue(tbm.size() == 32);
+	}
+
 	private Map<String, PhysicalDBPool> initDataHosts(ConfigLoader configLoader) {
 		Map<String, DataHostConfig> nodeConfs = configLoader.getDataHosts();
 		Map<String, PhysicalDBPool> nodes = new HashMap<String, PhysicalDBPool>(
@@ -104,8 +103,8 @@ public class ConfigTest {
 		}
 		return nodes;
 	}
-    
-    private PhysicalDatasource[] createDataSource(DataHostConfig conf,
+
+	private PhysicalDatasource[] createDataSource(DataHostConfig conf,
 			String hostName, String dbType, String dbDriver,
 			DBHostConfig[] nodes, boolean isRead) {
 		PhysicalDatasource[] dataSources = new PhysicalDatasource[nodes.length];
@@ -116,15 +115,7 @@ public class ConfigTest {
 				dataSources[i] = ds;
 			}
 
-		} else if(dbDriver.equals("jdbc"))
-			{
-			for (int i = 0; i < nodes.length; i++) {
-				nodes[i].setIdleTimeout(system.getIdleTimeout());
-				JDBCDatasource ds = new JDBCDatasource(nodes[i], conf, isRead);
-				dataSources[i] = ds;
-			}
-			}
-		else {
+		} else {
 			throw new ConfigException("not supported yet !" + hostName);
 		}
 		return dataSources;
@@ -145,8 +136,9 @@ public class ConfigTest {
 					dbType, dbDriver, entry.getValue(), true);
 			readSourcesMap.put(entry.getKey(), readSources);
 		}
-		PhysicalDBPool pool = new PhysicalDBPool(conf.getName(),conf, writeSources,
-				readSourcesMap, conf.getBalance(), conf.getWriteType());
+		PhysicalDBPool pool = new PhysicalDBPool(conf.getName(), conf,
+				writeSources, readSourcesMap, conf.getBalance(),
+				conf.getWriteType());
 		return pool;
 	}
 
